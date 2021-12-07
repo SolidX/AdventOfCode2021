586f8e7f4fa7e11d7fdff79a19db2a5be95b429d
SolidX <SolidX@users.noreply.github.com>
2021-12-06 20:26:05 -0500

    Day 03

---
+++ Day 03/AoC Day 03/AoC Day 03/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace AoC_Day_03
+{
+    public static class Extensions
+    {
+        public static long ToInt32(this BitArray x)
+        {
+            var value = 0L;
+            for (var i = 0; i < x.Length; i++)
+            {
+                if (x[i])
+                    value += (long)Math.Pow(2, 31 - i);
+            }
+            return value;
+        }
+
+        public static BitArray ToBitArray(this string s)
+        {
+            var uniqueChars = s.ToHashSet();
+            if ((uniqueChars.Contains('1') || uniqueChars.Contains('0')) && uniqueChars.Count() <= 2)
+            {
+                
+                var padding = new bool[32 - s.Length];
+                var mask = s.Select(x => x == '1');
+                return new BitArray(padding.Concat(mask).ToArray());
+            }
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ Day 03/AoC Day 03/AoC Day 03/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+
+namespace AoC_Day_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+==========================+");
+            Console.WriteLine("| Advent of Code -- Day 03 |");
+            Console.WriteLine("+==========================+");
+
+            var diagnosticReport = File.ReadAllLines("./input");
+            Part1(diagnosticReport);
+            Part2(diagnosticReport);
+        }
+
+        public static void Part1(string[] report)
+        {
+            Console.WriteLine("~ Part 1 ~");
+            Console.WriteLine();
+
+            var bits = report[0].Length;
+            var count = new int[bits];
+
+            foreach (var entry in report)
+            {
+                for(int i = 0; i < bits; i++)
+                {
+                    if (entry[i] == '1')
+                        count[i]++;
+                }
+            }
+
+            var padding = new bool[32 - bits];
+            var mask = count.Select(x => x > (report.Length / 2)).ToArray();
+
+            var gamma = new BitArray(padding.Concat(mask).ToArray());
+            var epsilon = new BitArray(padding.Concat(mask.Select(x => !x)).ToArray());
+
+            var powerUsage = gamma.ToInt32() * epsilon.ToInt32();
+
+            Console.WriteLine($"Power consumption: {powerUsage}");
+            Console.WriteLine();
+        }
+
+        public static void Part2(string[] report)
+        {
+            Console.WriteLine("~ Part 2 ~");
+            Console.WriteLine();
+
+            var bits = report[0].Length;
+            
+            var mostCommon = "";
+            var leastCommon = "";
+
+            var count = report.Count(x => x.StartsWith('1'));
+
+            if (count > report.Length / 2)
+            {
+                mostCommon += '1';
+                leastCommon += '0';
+            }
+            else
+            {
+                if (count < report.Length / 2)
+                {
+                    mostCommon += '0';
+                    leastCommon += '1';
+                }
+                else
+                {
+                    if (count == report.Length / 2)
+                    {
+                        mostCommon += '1';
+                        leastCommon += '0';
+                    }
+                }
+                
+            }
+
+            var searchSpace = report.Where(x => x.StartsWith(mostCommon));
+
+            do
+            {
+                var c = searchSpace.Count(x => x.StartsWith(mostCommon + '1'));
+                if (c >= (decimal)searchSpace.Count() / (decimal)2)
+                {
+                    mostCommon += '1';
+                }
+                else
+                {
+                    mostCommon += '0';
+                }
+
+                searchSpace = searchSpace.Where(x => x.StartsWith(mostCommon));
+            } while (mostCommon.Length < bits && searchSpace.Count() > 1);
+
+            var o2 = searchSpace.Single().ToBitArray().ToInt32();
+            Console.WriteLine($"Oxygen Generator Rating: {mostCommon} ({o2})");
+
+            searchSpace = report.Where(x => x.StartsWith(leastCommon));
+
+            do
+            {
+                var c = searchSpace.Count(x => x.StartsWith(leastCommon + '1'));
+                if (c < (decimal)searchSpace.Count() / (decimal)2)
+                {
+                    leastCommon += '1';
+                }
+                else
+                {
+                    leastCommon += '0';
+                }
+
+                searchSpace = searchSpace.Where(x => x.StartsWith(leastCommon));
+            } while (leastCommon.Length < bits && searchSpace.Count() > 1);
+
+            var co2 = searchSpace.Single().ToBitArray().ToInt32();
+            Console.WriteLine($"CO2 Scrubber Rating: {leastCommon} ({co2})");
+
+            Console.WriteLine($"Submarine Life Support rating: {o2 * co2}");
+            Console.WriteLine();
+        }
+    }
+}
