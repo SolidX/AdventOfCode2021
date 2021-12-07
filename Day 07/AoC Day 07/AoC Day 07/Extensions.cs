9966085a824693aba97e1f6579f9fe4dba2dd1d4
SolidX <SolidX@users.noreply.github.com>
2021-12-07 14:26:55 -0500

    Day 07

---
+++ Day 07/AoC Day 07/AoC Day 07/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day_07
+{
+    public static class Extensions
+    {
+        public static decimal Median(this IEnumerable<ushort> x)
+        {
+            var sorted = x.OrderBy(y => y).ToArray();
+            if (sorted.Length % 2 == 1)
+            {
+                return sorted[sorted.Length / 2];
+            }
+            else
+            {
+                return ((decimal)sorted[sorted.Length / 2 - 1] + (decimal)sorted[sorted.Length / 2]) / 2m;
+            }
+        }
+        public static IEnumerable<ushort> Mode(this IEnumerable<ushort> x)
+        {
+            var occurance = new Dictionary<ushort, ulong>();
+            foreach (var n in x)
+            {
+                if (occurance.ContainsKey(n))
+                    occurance[n]++;
+                else
+                    occurance.Add(n, 1);
+            }
+
+            var maxOccurances = occurance.Values.Max();
+            return occurance.Where(kvp => kvp.Value == maxOccurances).Select(kvp => kvp.Key);
+        }
+
+        public static long TriangularSum(this int n)
+        {
+            //I figured there was a name for this sequence and Wiki knew it
+            //https://en.wikipedia.org/wiki/Triangular_number
+
+            return (long)n * ((long)n + 1L) / 2L;
+        }
+    }
+}
---
+++ Day 07/AoC Day 07/AoC Day 07/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC_Day_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+==========================+");
+            Console.WriteLine("| Advent of Code -- Day 04 |");
+            Console.WriteLine("+==========================+");
+
+            var input = File.ReadAllLines("./input");
+            var crabPositions = input[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(n => UInt16.Parse(n)).ToList();
+            //var crabPositions = "16,1,2,0,4,2,7,1,2,14".Split(',', StringSplitOptions.RemoveEmptyEntries).Select(n => UInt16.Parse(n)).ToList();
+
+            Part1(crabPositions);
+            Part2(crabPositions);
+        }
+        public static void Part1(IEnumerable<ushort> positions)
+        {
+            Console.WriteLine("~ Part 1 ~");
+            Console.WriteLine();
+
+            var minPos = positions.Min();
+            var maxPos = positions.Max();
+            var median = positions.Median(); //I suspect this is the ideal spot but I can't mathematically prove that...
+
+            //...so, Brute force! (just to be safe)
+            var bestFuelConsumption = Int32.MaxValue;
+            var bestPos = -1;
+
+            for (var i = minPos; i <= maxPos; i++)
+            {
+                var fuelConsumption = 0;
+                foreach (var crab in positions)
+                    fuelConsumption += Math.Abs(crab - i);
+
+                if (fuelConsumption < bestFuelConsumption)
+                {
+                    bestPos = i;
+                    bestFuelConsumption = fuelConsumption;
+                }
+            }
+
+            Console.WriteLine($"Optimal position: {bestPos}");
+            Console.WriteLine($"Required Fuel Consumption: {bestFuelConsumption}");
+            Console.WriteLine();
+        }
+
+        public static void Part2(IEnumerable<ushort> positions)
+        {
+            Console.WriteLine("~ Part 2 ~");
+            Console.WriteLine();
+
+            var minPos = positions.Min();
+            var maxPos = positions.Max();
+
+            var bestFuelConsumption = Int64.MaxValue;
+            var bestPos = -1;
+
+            for (var i = minPos; i <= maxPos; i++)
+            {
+                var fuelConsumption = 0L;
+                foreach (var crab in positions)
+                    fuelConsumption += Math.Abs(crab - i).TriangularSum();
+
+                if (fuelConsumption < bestFuelConsumption)
+                {
+                    bestPos = i;
+                    bestFuelConsumption = fuelConsumption;
+                }
+            }
+
+            Console.WriteLine($"Optimal position: {bestPos}");
+            Console.WriteLine($"Required Fuel Consumption: {bestFuelConsumption}");
+            Console.WriteLine();
+        }
+    }
+}
