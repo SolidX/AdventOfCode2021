ed4f21f0ea962e2b3db7f96c0decaf86bb136ac4
SolidX <SolidX@users.noreply.github.com>
2021-12-13 14:57:52 -0500

    Day 13
    That was neat

---
+++ Day 13/AoC Day 13/AoC Day 13/FoldableGrid.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC_Day_13
+{
+    public class FoldableGrid
+    {
+        public List<Coordinate> Points { get; set; }
+        public Queue<Tuple<char, int>> FoldingInstructions { get; set; }
+
+        public void Fold()
+        {
+            if (FoldingInstructions.Count > 0)
+            {
+                var inst = FoldingInstructions.Dequeue();
+                switch (inst.Item1)
+                {
+                    case 'x':
+                        FoldLeft(inst.Item2);
+                        break;
+                    case 'y':
+                        FoldUp(inst.Item2);
+                        break;
+                }
+            }
+        }
+
+        public void FoldUp(int y)
+        {
+            foreach (var pt in Points)
+            {
+                if (y >= pt.Y)
+                    continue;
+
+                pt.Y = y - (pt.Y - y);
+            }
+            Points = Points.Distinct().ToList();
+        }
+        public void FoldLeft(int x)
+        {
+            foreach (var pt in Points)
+            {
+                if (x >= pt.X)
+                    continue;
+
+                pt.X = x - (pt.X - x);
+            }
+            Points = Points.Distinct().ToList();
+        }
+
+        public override string ToString()
+        {
+            var minX = Points.Min(pt => pt.X);
+            var minY = Points.Min(pt => pt.Y);
+            var maxX = Points.Max(pt => pt.X) + 1;
+            var maxY = Points.Max(pt => pt.Y) + 1;
+
+            var output = new char[maxY - minY][];
+
+            for (var i = 0; i < output.Length; i++)
+            {
+                var tmp = new char[maxX - minX];
+                Array.Fill(tmp, ' ');
+                output[i] = tmp;
+            }
+
+            foreach (var pt in Points)
+                output[pt.Y - minY][pt.X - minX] = '\u25A0'; 
+
+
+            var bld = new StringBuilder();
+            for (var i = 0; i < output.Length; i++)
+            {
+                bld.Append(new string(output[i]));
+                bld.Append(Environment.NewLine);
+            }
+
+            return bld.ToString();
+        }
+    }
+}
---
+++ Day 13/AoC Day 13/AoC Day 13/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC_Day_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+==========================+");
+            Console.WriteLine("| Advent of Code -- Day 13 |");
+            Console.WriteLine("+==========================+");
+
+            var input = File.ReadAllLines("./input");
+
+            Part1(ParseInput(input));
+            Part2(ParseInput(input));
+        }
+
+        public static FoldableGrid ParseInput(string[] input)
+        {
+            var coordinateRegex = new Regex(@"([0-9]+),([0-9]+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var foldRegex = new Regex(@"fold along (x|y)=([0-9]+)", RegexOptions.Compiled);
+
+            var coords = new List<Coordinate>();
+            var instructions = new Queue<Tuple<char, int>>();
+
+            foreach (var line in input)
+            {
+                if (coordinateRegex.IsMatch(line))
+                {
+                    var matches = coordinateRegex.Match(line);
+                    var c = new Coordinate(Int32.Parse(matches.Groups[1].Value), Int32.Parse(matches.Groups[2].Value));
+                    coords.Add(c);
+                    continue;
+                }
+                if (foldRegex.IsMatch(line))
+                {
+                    var matches = foldRegex.Match(line);
+                    var i = new Tuple<char, int>(matches.Groups[1].Value.First(), Int32.Parse(matches.Groups[2].Value));
+                    instructions.Enqueue(i);
+                    continue;
+                }
+            }
+
+            return new FoldableGrid { FoldingInstructions = instructions, Points = coords };
+        }
+
+        public static void Part1(FoldableGrid g)
+        {
+            Console.WriteLine("~ Part 1 ~");
+            Console.WriteLine();
+
+            g.Fold();
+
+            Console.WriteLine($"Dots Visible After 1 Fold: {g.Points.Count()}");
+            Console.WriteLine();
+        }
+
+        public static void Part2(FoldableGrid g)
+        {
+            Console.WriteLine("~ Part 2 ~");
+            Console.WriteLine();
+            
+            var output = new List<string>();
+            while (g.FoldingInstructions.Count() > 0)
+            {
+                g.Fold();
+                output.Add(g.ToString());
+            }
+
+            using (var f = new StreamWriter("output.txt"))
+            {
+                f.WriteLine(output.Last());
+            }
+
+            Console.WriteLine("Activation Key:");
+            Console.WriteLine(output.Last());
+            Console.WriteLine();
+        }
+    }
+}
